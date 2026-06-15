aec69766306219f13b75f335d83188ebb86d03d0
bhupesh <bhupeshverma29bv@gmail.com>
2026-06-15 15:57:02 +0530

    FIX: string to number every where

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -4,3 +4,14 @@ import { PrismaClient } from "./generated/prisma/client";
 const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaPg({ connectionString });
 export const prisma = new PrismaClient({ adapter });
+
+// price      -> Int
+// qty        -> Int
+// margin     -> Int
+// balance    -> Int
+// pnl        -> Int
+
+// 100.25     => 10025
+// 1000 USDT  => 100000 (if scale=100)
+
+// All calculations are integer math.
